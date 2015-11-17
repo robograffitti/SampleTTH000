870fd052952793ec84f285aaca9f0b8e6845674c
yoshimalucky <y-tanaka@jsk.imi.i.u-tokyo.ac.jp>
2015-11-17 23:09:50 +0900

    Initial commit.

---
+++ app/src/main/java/com/example/yoshimalucky/sampletth000/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.yoshimalucky.sampletth000;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button btn = (Button) findViewById(R.id.button); // Button btn = new Button(this);
+        btn.setText("Hello");
+        mediaPlayer = MediaPlayer.create(this, R.raw.intel_cm_sound_352609);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button button = (Button) view;
+                button.setText("こんにちは。");
+                mediaPlayer.start();
+                // how to use intent: Screen Transition
+                Intent intent = new Intent(MainActivity.this, NextActivity.class);
+                // intent.putExtra("vx", 10);
+                startActivity(intent);
+            }
+        }); // setContentView(btn);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/yoshimalucky/sampletth000/NextActivity.java
@@ -0,0 +1,17 @@
+package com.example.yoshimalucky.sampletth000;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by y-tanaka on 2015/11/17.
+ */
+public class NextActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = new View(this);
+        setContentView(view);
+    }
+}
